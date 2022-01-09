77cf4ab9b46efaf3bdc9c3bd0a5dce9962004d57
Max <egorovteam2019@yandex.ru>
2022-01-09 03:11:38 +0300

    hw-5 refactor

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -5,31 +5,23 @@ import PreJunior from "./pages/PreJunior";
 import Junior from "./pages/Junior";
 import JuniorPlus from "./pages/Junior-plus";
 
-export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus'
-}
-
-
 function RoutesHw() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
 
-                <Routes>
-                    {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                    {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-                    <Route path={'/*'} element={<Error404/>}/>
-                    <Route path={'/'} element={<PreJunior/>}/>
-                    <Route path={'/pre-junior'} element={<PreJunior/>}/>
-                    <Route path={'/junior'} element={<Junior/>}/>
-                    <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
-                    // add routes
+            <Routes>
+                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
+                {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
+                <Route path={'/*'} element={<Error404/>}/>
+                <Route path={'/pre-junior'} element={<PreJunior/>}/>
+                <Route path={'/junior'} element={<Junior/>}/>
+                <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
+                // add routes
 
-                    {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
 
-                </Routes>
+            </Routes>
         </div>
     )
 }
